e70c08693da84f18dc7e340ed77c44a4c059c38c
Dhruthisk1607 <eng24am0164@dsu.edu.in>
2026-04-12 18:42:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,49 +1,9 @@
-import gradio as gr
-from cleaner import CSVDataCleanerAgent
+from environment import app
+import uvicorn
 
-# initialize agent once (important)
-agent = CSVDataCleanerAgent("messy_data.csv")
-
-
-def run_action(action):
-    try:
-        action = action.strip().lower()
-
-        if action == "remove_nulls":
-            return {
-                "result": agent.remove_nulls()
-            }
-
-        elif action == "drop_duplicates":
-            return {
-                "result": agent.drop_duplicates()
-            }
-
-        elif action == "show_head":
-            return {
-                "result": agent.show_head()
-            }
-
-        elif action == "get_info":
-            return {
-                "result": agent.get_info()
-            }
-
-        else:
-            return {
-                "error": "Invalid action. Use: remove_nulls, drop_duplicates, show_head, get_info"
-            }
-
-    except Exception as e:
-        return {"error": str(e)}
+def main():
+    uvicorn.run(app, host="0.0.0.0", port=7860)
 
 
-demo = gr.Interface(
-    fn=run_action,
-    inputs="text",
-    outputs="json",
-    title="CSV Cleaner Agent",
-    description="Actions: remove_nulls, drop_duplicates, show_head, get_info"
-)
-
-demo.launch()
+if __name__ == "__main__":
+    main()
