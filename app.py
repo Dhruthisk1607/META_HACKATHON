48d4e7491f9c28afd35ed94689b149bcc3a325e9
Dhruthisk1607 <eng24am0164@dsu.edu.in>
2026-04-12 19:01:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
-from environment import app
+from fastapi import FastAPI
 import uvicorn
 
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "CSV Cleaner API is running"}
+
+
+# ---------------- MAIN ENTRY POINT ----------------
 def main():
-    uvicorn.run(app, host="0.0.0.0", port=7860)
+    uvicorn.run(
+        "server.app:app",
+        host="0.0.0.0",
+        port=7860,
+        reload=True
+    )
 
 
 if __name__ == "__main__":
